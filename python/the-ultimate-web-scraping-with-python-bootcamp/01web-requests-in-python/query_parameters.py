99e72975d43154d76a69571d0298d86b55d26736
Antonio <antonioreverso.com@gmail.com>
2025-01-10 00:16:55 -0300

    stop-new

--- python/the-ultimate-web-scraping-with-python-bootcamp/01web-requests-in-python/query_parameters.py
+++ python/the-ultimate-web-scraping-with-python-bootcamp/01web-requests-in-python/query_parameters.py
@@ -23,7 +23,8 @@ params = {
 }
 
 headers = {
-
+#Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:129.0) Gecko/20100101 Firefox/129.0
+    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:129.0) Gecko/20100101 Firefox/129.0"
 }
 
 
@@ -32,3 +33,5 @@ resp = r.get(url, params=params)
 print(resp.status_code)
 
 print(resp.request.url)
+
+
