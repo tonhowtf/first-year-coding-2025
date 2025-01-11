c00c2b172a31728ec9de0e37eb7f94c7ff1bddb0
Antonio <antonioreverso.com@gmail.com>
2025-01-11 01:53:32 -0300

    feat: value02

---
+++ java/Java10x/JavaNinja/src/Basico/Array.java
@@ -0,0 +1,13 @@
+package Basico;
+
+public class Array {
+    public static void main(String[] args) {
+
+        /*String ninja1 = "Naruto Uzumaki";
+        String ninja2 = "Sasuke Uchiha";
+        String ninja3 = "Sakura Haruno";
+        */
+
+        String[3] ninja;
+    }
+}
--- java/Java10x/JavaNinja/src/Condicoes/LacosDeRepeticao.java
+++ java/Java10x/JavaNinja/src/Condicoes/LacosDeRepeticao.java
@@ -3,13 +3,18 @@ package Condicoes;
 public class LacosDeRepeticao {
     public static void main(String[] args) {
 
-    int numeroDeClones = 0;
+    int numeroDeClones = 3;
     int numeroMaximoDeClones = 40;
 
     while (numeroDeClones <= numeroMaximoDeClones){
         System.out.println("O Naruto fez um clone das sombras");
+        numeroDeClones ++;
     }
-    numeroDeClones ++;
+
+        for (int i = 0; i <= numeroMaximoDeClones; i++) {
+            System.out.println("Naruto se clonando " + i);
+        }
+
 
 
     }
