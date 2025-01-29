686ec7b1fabd3e18ea369606403623897cb07fa0
Tonho <antonioreverso.com@gmail.com>
2025-01-29 15:38:41 -0300

    feat: update script references and enhance JavaScript examples in index.html and script.js

--- javascript/Jonas-Schmedtmann/The-Complete-JavaScript-Course-2025-From-Zero-to-Expert/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ javascript/Jonas-Schmedtmann/The-Complete-JavaScript-Course-2025-From-Zero-to-Expert/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,8 +1,22 @@
 let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
-console.log("Jonas");
-console.log(23)
+console.log("Tonho");
+console.log(56);
 
-let firstName = "Jonas"
-console.log(firstName)
+let firstName = 'tonhoca';
+
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+let name = "Jonas";
+let person = 'jonas';
+let PI = 3.1415;
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
