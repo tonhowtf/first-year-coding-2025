557e56069d447edab52ba6ef301875d0055c700c
Tonho <antonioreverso.com@gmail.com>
2025-01-31 19:58:55 -0300

    feat: update script reference in index.html and comment out code in script.js; add new Data Types challenge file

--- javascript/Jonas-Schmedtmann/The-Complete-JavaScript-Course-2025-From-Zero-to-Expert/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ javascript/Jonas-Schmedtmann/The-Complete-JavaScript-Course-2025-From-Zero-to-Expert/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,22 +1,50 @@
-let js = "amazing";
-console.log(40 + 8 + 23 - 10);
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
 
-console.log("Tonho");
-console.log(56);
+// console.log("Tonho");
+// console.log(56);
 
-let firstName = 'tonhoca';
+// let firstName = 'tonhoca';
 
-console.log(firstName);
-// 
-let jonas_matilda = "JM";
-let $function = 27;
-let name = "Jonas";
-let person = 'jonas';
-let PI = 3.1415;
-let myFirstJob = 'Programmer';
-let myCurrentJob = 'Teacher';
+// console.log(firstName);
+// // 
+// let jonas_matilda = "JM";
+// let $function = 27;
+// let name = "Jonas";
+// let person = 'jonas';
+// let PI = 3.1415;
+// let myFirstJob = 'Programmer';
+// let myCurrentJob = 'Teacher';
 
-let job1 = 'programmer';
-let job2 = 'teacher';
+// let job1 = 'programmer';
+// let job2 = 'teacher';
 
-console.log(myFirstJob);
+// console.log(myFirstJob);
+
+// true;
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+// javascriptIsFun = 'Yes!';
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1991;
+// console.log(typeof year);
+
+// console.log(typeof null)
+
+let age = 30;
+age = 31;
+const birthYear = 1991;
+birthYear = 1990;
+
+var job = 'programmer';
+job = 'teacher'
