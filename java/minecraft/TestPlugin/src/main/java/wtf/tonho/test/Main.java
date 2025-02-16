9e9f41414356f458e2175b4aaca839b21c5bb4f0
Tonho <antonioreverso.com@gmail.com>
2025-02-16 02:31:59 -0300

    refactor: remove config initialization from Minecraft plugin

--- java/minecraft/TestPlugin/src/main/java/wtf/tonho/test/Main.java
+++ java/minecraft/TestPlugin/src/main/java/wtf/tonho/test/Main.java
@@ -8,11 +8,6 @@ public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable(){
-
-        getConfig().options().copyDefaults();
-        saveDefaultConfig();
-
-
-        getCommand("config").setExecutor(new ConfigCommand(this));
+        
     }
 }
