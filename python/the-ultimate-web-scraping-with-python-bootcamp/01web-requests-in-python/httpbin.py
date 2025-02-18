3d8572f64bddcf5c2cbe452d68100bb8aca94291
Tonho <antonioreverso.com@gmail.com>
2025-02-17 22:11:34 -0300

    chore: remove web scraping example scripts from Python course

--- python/the-ultimate-web-scraping-with-python-bootcamp/01web-requests-in-python/httpbin.py
+++
@@ -1,5 +0,0 @@
-import requests as r
-
-print(r.delete("https://www.httpbin.org/delete").json())
-
-print(r.patch("https://www.httpbin.org/patch").json())
--- python/the-ultimate-web-scraping-with-python-bootcamp/01web-requests-in-python/urllib_curso.py
+++
@@ -1,7 +0,0 @@
-from urllib.request import urlopen
-
-url = "https://quotes.toscrape.com/"
-
-with urlopen(url) as response:
-    content = response.read()
-    print(content.decode("utf-8"))
