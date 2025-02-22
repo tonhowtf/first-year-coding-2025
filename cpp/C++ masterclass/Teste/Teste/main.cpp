2d206ad289435edbfdfe42b542bd86e76a6730bf
Tonho <antonioreverso.com@gmail.com>
2025-02-22 16:24:11 -0300

    feat: replace constant with enum for month representation and update output logic

--- cpp/C++ masterclass/Teste/Teste/main.cpp
+++ cpp/C++ masterclass/Teste/Teste/main.cpp
@@ -2,21 +2,59 @@
 
 using namespace std;
 
-#define LAST_MONTH 12
+enum class Months : uint8_t {
+	January = 1,
+	February,
+	March,
+	April,
+	May,
+	June,
+	July,
+	August,
+	September,
+	October,
+	November,
+	December
+};
 
 int main()
 {
-	const int screenMaxXPos = 80;
-	int x = screenMaxXPos;
+	Months currentMonth = Months::December;
 
-	const int lastMonth = LAST_MONTH;
-	
-
-	int currentMonth = LAST_MONTH;
-
-	if (currentMonth == lastMonth)
-	{
-		cout << "December!\n";
+	cout << sizeof(Months) << endl;
+	cout << "Number: " << static_cast<int>(currentMonth) << endl;
+	if (currentMonth == Months::January) {
+		cout << "January!" << endl;
+	}
+	else if (currentMonth == Months::February) {
+		cout << "February!" << endl;
+	}
+	else if (currentMonth == Months::March) {
+		cout << "March!" << endl;
+	}
+	else if (currentMonth == Months::April) {
+		cout << "April" << endl;
+	}
+	else if (currentMonth == Months::May) {
+		cout << "May!" << endl;
+	}
+	else if (currentMonth == Months::July) {
+		cout << "July!" << endl;
+	}
+	else if (currentMonth == Months::August) {
+		cout << "August!" << endl;
+	}
+	else if (currentMonth == Months::September) {
+		cout << "September!" << endl;
+	}
+	else if (currentMonth == Months::October) {
+		cout << "October!" << endl;
+	}
+	else if (currentMonth == Months::November) {
+		cout << "November!" << endl;
+	}
+	else if (currentMonth == Months::December) {
+		cout << "December!" << endl;
 	}
 
 	return 0;
