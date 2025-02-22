9db5df84da9ee989001bafb4657263938b323a0c
Tonho <antonioreverso.com@gmail.com>
2025-02-21 23:40:36 -0300

    feat: add constants and conditional check for December month

--- cpp/C++ masterclass/Teste/Teste/main.cpp
+++ cpp/C++ masterclass/Teste/Teste/main.cpp
@@ -2,9 +2,22 @@
 
 using namespace std;
 
+#define LAST_MONTH 12
+
 int main()
 {
-	float grade = 0;
-	grade = 10;
+	const int screenMaxXPos = 80;
+	int x = screenMaxXPos;
+
+	const int lastMonth = LAST_MONTH;
+	
+
+	int currentMonth = LAST_MONTH;
+
+	if (currentMonth == lastMonth)
+	{
+		cout << "December!\n";
+	}
+
 	return 0;
 }
