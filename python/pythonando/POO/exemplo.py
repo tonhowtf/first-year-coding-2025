c201a8b3065854cb7bee3c7a26770d4ea1efe4d6
Tonho <antonioreverso.com@gmail.com>
2025-02-23 00:44:47 -0300

    feat: comment out print statements and variable assignments in example script

--- python/pythonando/POO/exemplo.py
+++ python/pythonando/POO/exemplo.py
@@ -7,19 +7,19 @@
 #     {"name": "Gyarados", "type": "water/Flying", "hp": 95}
 # ]
 
-high_hp_pokemon = [pokemon["name"] for pokemon in pokemon_list if pokemon["hp"] > 50]
+# high_hp_pokemon = [pokemon["name"] for pokemon in pokemon_list if pokemon["hp"] > 50]
 
-print(high_hp_pokemon)
+# print(high_hp_pokemon)
 
-memory_addresses = [0x1000, 0x1004, 0x1010, 0x1018, 0x1020, 0x1024, 0x1030]
+# memory_addresses = [0x1000, 0x1004, 0x1010, 0x1018, 0x1020, 0x1024, 0x1030]
 
-aligned_address = [hex(addr) for addr in memory_addresses if addr % 0x10 == 0]
+# aligned_address = [hex(addr) for addr in memory_addresses if addr % 0x10 == 0]
 
-print(aligned_address)
+# print(aligned_address)
 
 
-binary_data = [0x41, 0x42, 0x03, 0x7A, 0x20, 0x2E, 0x10, 0x65, 0x66]
+# binary_data = [0x41, 0x42, 0x03, 0x7A, 0x20, 0x2E, 0x10, 0x65, 0x66]
 
-printable_chars = [chr(b) for b in binary_data if 32 <= b <= 126]
+# printable_chars = [chr(b) for b in binary_data if 32 <= b <= 126]
 
-print(printable_chars)
+# print(printable_chars)
