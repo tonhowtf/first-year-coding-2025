f477c7c10c7d37263bb9e542049f5761799d5511
Tonho <antonioreverso.com@gmail.com>
2025-02-26 00:39:44 -0300

    fix: update BeautifulSoup initialization to use response content and prettify output

--- python/the-ultimate-web-scraping-with-python-bootcamp/web-requests-in-python/app.py
+++ python/the-ultimate-web-scraping-with-python-bootcamp/web-requests-in-python/app.py
@@ -3,6 +3,6 @@ from bs4 import BeautifulSoup
 
 resp = r.get("https://books.toscrape.com/")
 
-soup = BeautifulSoup(resp)
+soup = BeautifulSoup(resp.content, "html.parser")
 
-print(soup.ul)
+print(soup.ul.prettify)
