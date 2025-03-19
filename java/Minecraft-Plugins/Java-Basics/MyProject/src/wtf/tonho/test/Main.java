316f568bc7cf9feaa9c39a3a737d2b3d029e2aca
Tonho <antonioreverso.com@gmail.com>
2025-03-19 19:10:35 -0300

    chore: update project configuration to use JDK 21 and add Main class with example code

---
+++ java/Minecraft-Plugins/Java-Basics/MyProject/src/wtf/tonho/test/Main.java
@@ -0,0 +1,23 @@
+package wtf.tonho.test;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        // PRIMITIVE
+        int number = 5;
+        double decimalNumber = 102.5;
+        boolean trueFalse = false;
+        char letter = 'L';
+
+       int result = printMessage("tessadast");
+
+    }
+
+    private static int printMessage(String message) {
+        System.out.println(message);
+        return 12;
+    }
+
+}
