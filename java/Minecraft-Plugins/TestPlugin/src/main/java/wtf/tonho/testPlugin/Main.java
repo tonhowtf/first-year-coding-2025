9f5f7f6dc96b381d6af0c7980389ed6ba342dac7
Tonho <antonioreverso.com@gmail.com>
2025-03-29 17:49:39 -0300

    feat: implement HealCommand class and update Main to register the command; rename TestPlugin to testplugin for consistency

--- java/Minecraft-Plugins/TestPlugin/src/main/java/wtf/tonho/testPlugin/Main.java
+++ java/Minecraft-Plugins/TestPlugin/src/main/java/wtf/tonho/testPlugin/Main.java
@@ -1,17 +1,21 @@
 package wtf.tonho.testPlugin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.PlayerEggThrowEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
 
-public final class TestPlugin extends JavaPlugin {
+public final class Main extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        
-
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+        getCommand("heal").setExecutor(new HealCommand());
     }
 }
