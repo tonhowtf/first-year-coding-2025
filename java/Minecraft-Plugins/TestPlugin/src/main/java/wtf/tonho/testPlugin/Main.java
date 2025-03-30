c5e0e8e3d91090aa9d8f84887029109ca70d75e9
Tonho <antonioreverso.com@gmail.com>
2025-03-30 13:44:01 -0300

    fix: clean up pom.xml and Main class; remove unnecessary whitespace and imports

--- java/Minecraft-Plugins/TestPlugin/src/main/java/wtf/tonho/testPlugin/Main.java
+++ java/Minecraft-Plugins/TestPlugin/src/main/java/wtf/tonho/testPlugin/Main.java
@@ -1,21 +1,11 @@
 package wtf.tonho.testPlugin;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.player.PlayerEggThrowEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.util.Vector;
 
-public final class Main extends JavaPlugin implements Listener {
+public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getCommand("heal").setExecutor(new HealCommand());
+        getCommand("test").setExecutor(new TestCommand);
     }
 }
