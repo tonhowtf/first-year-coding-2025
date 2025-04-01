5c9d1ee0ace1431eddfb29186958a1d3eb59d895
Tonho <antonioreverso.com@gmail.com>
2025-04-01 07:27:46 -0300

    feat: implement player join event to give custom leather armor

--- java/Minecraft-Plugins/TestPlugin/src/main/java/wtf/tonho/testPlugin/Main.java
+++ java/Minecraft-Plugins/TestPlugin/src/main/java/wtf/tonho/testPlugin/Main.java
@@ -1,11 +1,39 @@
 package wtf.tonho.testPlugin;
-
+import org.bukkit.Bukkit;
+import org.bukkit.Color;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.LeatherArmorMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
+public class Main extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        getCommand("test").setExecutor(new TestCommand());
+        Bukkit.getPluginManager().registerEvents(this, this);
+
     }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e){
+
+        ItemStack helmet = new ItemStack(Material.LEATHER_HELMET);
+        LeatherArmorMeta helmetMeta = (LeatherArmorMeta) helmet.getItemMeta();
+        helmetMeta.setColor(Color.fromARGB(255, 106, 13, 173));
+        helmet.setItemMeta(helmetMeta);
+
+        e.getPlayer().getInventory().addItem(helmet);
+
+        ItemStack chestplate = new ItemStack(Material.LEATHER_CHESTPLATE);
+        LeatherArmorMeta chestplateMeta = (LeatherArmorMeta) chestplate.getItemMeta();
+        chestplateMeta.setColor(Color.fromARGB(255, 0, 150, 136));
+        chestplate.setItemMeta(chestplateMeta);
+
+        e.getPlayer().getInventory().addItem(chestplate);
+
+    }
+
 }
