28db2b04c690e28841856d75114fe32719dc990d
Tonho <antonioreverso.com@gmail.com>
2025-04-01 21:59:58 -0300

    feat: update player join event to send action bar message

--- java/Minecraft-Plugins/TestPlugin/src/main/java/wtf/tonho/testPlugin/Main.java
+++ java/Minecraft-Plugins/TestPlugin/src/main/java/wtf/tonho/testPlugin/Main.java
@@ -1,12 +1,11 @@
 package wtf.tonho.testPlugin;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
-import org.bukkit.Color;
-import org.bukkit.Material;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.LeatherArmorMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
@@ -14,25 +13,12 @@ public class Main extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
-
     }
 
     @EventHandler
-    public void onJoin(PlayerJoinEvent e){
+    public void onJoin(PlayerJoinEvent e) {
 
-        ItemStack helmet = new ItemStack(Material.LEATHER_HELMET);
-        LeatherArmorMeta helmetMeta = (LeatherArmorMeta) helmet.getItemMeta();
-        helmetMeta.setColor(Color.fromARGB(255, 106, 13, 173));
-        helmet.setItemMeta(helmetMeta);
-
-        e.getPlayer().getInventory().addItem(helmet);
-
-        ItemStack chestplate = new ItemStack(Material.LEATHER_CHESTPLATE);
-        LeatherArmorMeta chestplateMeta = (LeatherArmorMeta) chestplate.getItemMeta();
-        chestplateMeta.setColor(Color.fromARGB(255, 0, 150, 136));
-        chestplate.setItemMeta(chestplateMeta);
-
-        e.getPlayer().getInventory().addItem(chestplate);
+        e.getPlayer().spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText("§4§lDale"));
 
     }
 
