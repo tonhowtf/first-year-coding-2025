dba3737fbd9e5b4bd5b83ef502178224c35c66c2
Tonho <antonioreverso.com@gmail.com>
2025-04-02 20:20:31 -0300

    feat: implement arena creation command with validation and glowstone structure

--- java/Minecraft-Plugins/TestPlugin/src/main/java/wtf/tonho/testPlugin/Main.java
+++ java/Minecraft-Plugins/TestPlugin/src/main/java/wtf/tonho/testPlugin/Main.java
@@ -1,25 +1,14 @@
 package wtf.tonho.testPlugin;
-import net.md_5.bungee.api.ChatMessageType;
-import net.md_5.bungee.api.chat.TextComponent;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin implements Listener {
+public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        Bukkit.getPluginManager().registerEvents(this, this);
+        getCommand("arena").setExecutor(new ArenaCommand());
+        getLogger().info("Arena ativado.com");
     }
-
-    @EventHandler
-    public void onJoin(PlayerJoinEvent e) {
-
-        e.getPlayer().spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText("§4§lDale"));
-
-    }
-
 }
+
+
