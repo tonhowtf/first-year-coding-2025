d75a9699c8be7ae234d6be43a34faa96b5782bf7
Tonho <antonioreverso.com@gmail.com>
2025-04-08 18:25:33 -0300

    feat: implement main method in List and RestoDoIntermediario classes with ninja array and list operations

--- java/java10x/JavaNinja/src/List/Main.java
+++ java/java10x/JavaNinja/src/List/Main.java
@@ -1,4 +1,29 @@
 package List;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+
+        // Array
+        String[] ninjasArray = new String[3];
+        ninjasArray[0] = "Ninjas";
+        ninjasArray[1] = "Bruno";
+        ninjasArray[2] = "Louco";
+
+        List<String> ninjasList = new ArrayList<>();
+        ninjasList.add("Naruto Uzumaki");
+        ninjasList.add("Naruto Uchiha");
+        ninjasList.add("Naruto Hyuuga");
+
+        System.out.println("Ninjas: " + ninjasList);
+
+        ninjasList.remove("Naruto Uzumaki");
+        System.out.println("Ninjas: " + ninjasList);
+
+        // Trocar elementos
+
+        System.out.println(ninjasList.get(3));
+    }
 }
--- java/java10x/JavaNinja/src/RestoDoIntermediario/Main.java
+++ java/java10x/JavaNinja/src/RestoDoIntermediario/Main.java
@@ -1,4 +1,10 @@
 package RestoDoIntermediario;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        
+
+    }
 }
