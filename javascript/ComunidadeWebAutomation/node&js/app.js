7315d5ae26c66ff6238d880b184cad5317562604
Tonho <antonioreverso.com@gmail.com>
2025-05-01 22:00:46 -0300

    Reintroduce app.js with initial variable declarations and console log

---
+++ javascript/ComunidadeWebAutomation/node&js/app.js
@@ -0,0 +1,10 @@
+// const nome = 'Tonho'
+// console.log(nome)
+
+
+const nome = 'Tonho'
+const idade = 3
+const altura = 1
+const sobrenome = aburgi
+
+console.log('Seu nome: ' + nome + ' sua idade: ' + idade + ' sua altura: ' + altura + ' Seu sobrenome: ' +sobrenome)
