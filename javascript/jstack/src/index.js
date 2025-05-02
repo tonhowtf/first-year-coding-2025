40c4fa412c062cb3a083f7d64654940424b83057
Tonho <antonioreverso.com@gmail.com>
2025-05-02 15:42:59 -0300

    Add user mock data and implement user retrieval endpoint

--- javascript/jstack/src/index.js
+++ javascript/jstack/src/index.js
@@ -1,8 +1,17 @@
 const http = require('http');
 
+const users = require('./mocks/users')
+
 const server = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.end('<h1>Hello world!</h1>');
+  console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
+
+  if (request.url === '/users' && request.method === 'GET') {
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.end(JSON.stringify(users))
+  }
+
+  // response.writeHead(200, { 'Content-Type': 'text/html' });
+  // response.end('<h1>Hello world!</h1>');
 });
 
 server.listen(3000, () => console.log('🥵 Server started at http://localhost:3000'))
---
+++ javascript/jstack/src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Tonho'
+  },
+  {
+    id: 1,
+    name: 'Landerberg'
+  },
+  {
+    id: 3,
+    name: 'Bruno'
+  }
+]
