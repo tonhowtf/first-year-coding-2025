c056484bd0f5a63d8024ef5b028d36a431fba9ca
Tonho <antonioreverso.com@gmail.com>
2025-05-02 18:19:26 -0300

    Refactor index.js: update server request handling and prepare for user route integration

--- javascript/jstack/src/index.js
+++ javascript/jstack/src/index.js
@@ -1,14 +1,24 @@
 const http = require('http');
 
-const users = require('./mocks/users')
+
+
+
 
 const server = http.createServer((request, response) => {
   console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
 
-  if (request.url === '/users' && request.method === 'GET') {
-    response.writeHead(200, { 'Content-Type': 'application/json' });
-    response.end(JSON.stringify(users))
-  }
+
+  const route = route.find((routeObj) => (
+    routeObj.endpoint === Request.url && routeObj.method === request.method
+  ));
+
+  // if (request.url === '/users' && request.method === 'GET') {
+  //   UserController.listUsers(request, response)
+
+  // } else {
+  //   response.writeHead(404, { 'Content-type': 'text/html' });
+  //   response.end('<h1>Erro 404</h1>')
+  // }
 
   // response.writeHead(200, { 'Content-Type': 'text/html' });
   // response.end('<h1>Hello world!</h1>');
