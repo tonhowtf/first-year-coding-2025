c3041fbfcb885516514e02f254acd762c531bcd6
Tonho <antonioreverso.com@gmail.com>
2025-05-02 18:19:29 -0300

    Add route definitions for user and product endpoints

---
+++ javascript/jstack/src/route.js
@@ -0,0 +1,16 @@
+const UserController = require('./controllers/UserController')
+
+module.exports = [
+  {
+    endpoint: '/users',
+    method: 'GET',
+    handler: UserController.listUsers,
+
+
+  },
+  {
+    endpoint: '/produtos',
+    method: 'GET',
+    handler: UserController.listUsers,
+  },
+];
