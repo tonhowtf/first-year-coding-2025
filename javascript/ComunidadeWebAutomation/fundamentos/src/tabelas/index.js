57dce06ad035a41367c9d9c4667e5d099e8b13dd
Tonho <antonioreverso.com@gmail.com>
2025-05-03 17:44:57 -0300

    Add index.js: implement data extraction from HTML using axios and cheerio

---
+++ javascript/ComunidadeWebAutomation/fundamentos/src/tabelas/index.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+const cheerio = require('cheerio')
+
+// https://pedroalpacheco.github.io/htmlpurocurso/
+
+axios.get('https://pedroalpacheco.github.io/htmlpurocurso/')
+  .then(resp => {
+    let dadoshtml = resp.data;
+    const $ = cheerio.load(dadoshtml)
+
+    const colunas = [];
+    $('[class="table"]>tbody>tr')
+      .each((i, e) => {
+        const todos = $(e).find('td')
+        const pais = $(todos[0]).text();
+        const previsao = $(todos[1]).text();
+        const referencia = $(todos[2]).text();
+
+        const coluna = { pais, previsao, referencia }
+        colunas.push(coluna)
+
+      })
+    console.log(colunas)
+  })
