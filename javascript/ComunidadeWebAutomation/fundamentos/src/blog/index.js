ffcf21911f8f0dd0ee0099300e8b5bcb074a5ed0
Tonho <antonioreverso.com@gmail.com>
2025-05-04 10:02:56 -0300

    Refactor index.js: update axios call to fetch data from the correct HTML source and clean up code structure

--- javascript/ComunidadeWebAutomation/fundamentos/src/blog/index.js
+++ javascript/ComunidadeWebAutomation/fundamentos/src/blog/index.js
@@ -1,11 +1,21 @@
 const axios = require('axios');
 const cheerio = require('cheerio')
 
-//https://www.gov.br/pagina-interna-noticias-ebc
-
-axios.get('https://www.gov.br/pagina-interna-noticias-ebc')
+//https://www.gov.br/mds/pt-br/noticias-e-conteudos/noticias
+axios.get('https://www.gov.br/mds/pt-br/noticias-e-conteudos/noticias')
   .then(resp => {
     let dadoshtml = resp.data;
-    const $ = cheerio.load(dadoshtml)
+    const $ = cheerio.load(dadoshtml);
 
+    noticias = []
+    $('a.summary.url')
+      .each((_, e) => {
+        const titulo = $(e).text().trim();
+        const link = $(e).attr('href');
+        noticias.push({ titulo, link })
+
+      });
+    console.log(noticias);
   })
+
+
