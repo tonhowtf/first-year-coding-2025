4add877e795bda058aeba2a961214d22e715f38c
Tonho <antonioreverso.com@gmail.com>
2025-05-03 22:01:00 -0300

    Refactor index.js: remove unused code and clean up axios call structure

--- javascript/ComunidadeWebAutomation/fundamentos/src/blog/index.js
+++ javascript/ComunidadeWebAutomation/fundamentos/src/blog/index.js
@@ -3,22 +3,9 @@ const cheerio = require('cheerio')
 
 //https://www.gov.br/pagina-interna-noticias-ebc
 
-axios.get('https://pedroalpacheco.github.io/htmlpurocurso/')
+axios.get('https://www.gov.br/pagina-interna-noticias-ebc')
   .then(resp => {
     let dadoshtml = resp.data;
     const $ = cheerio.load(dadoshtml)
 
-    const colunas = [];
-    $('[class="table"]>tbody>tr')
-      .each((i, e) => {
-        const todos = $(e).find('td')
-        const pais = $(todos[0]).text();
-        const previsao = $(todos[1]).text();
-        const referencia = $(todos[2]).text();
-
-        const coluna = { pais, previsao, referencia }
-        colunas.push(coluna)
-
-      })
-    console.log(colunas)
   })
