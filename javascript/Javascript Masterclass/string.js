8290c29b1abd39545ac6a18c131b859f6b2d7548
Tonho <antonioreverso.com@gmail.com>
2025-05-06 15:55:16 -0300

    Feat: add URL construction examples to string.js

--- javascript/Javascript Masterclass/string.js
+++ javascript/Javascript Masterclass/string.js
@@ -16,3 +16,10 @@ while (counter < 10000) {
 console.timeEnd("performance");
 let daysOfWeek = "0 - Sun\n1 - Mon \n2 - Tue\n3 - Wed\n4 - Thu\n5 - Fri\n6 - Sat ";
 console.log(daysOfWeek);
+let host = "localhost";
+let port = "3000";
+let resource = "users";
+let url = "https://" + host + ":" + port + "/" + resource;
+console.log(url)
+let url2 = `https://${host}:${port}/${resource}`;
+console.log(url2)
