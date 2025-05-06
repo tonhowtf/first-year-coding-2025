09f7ad8a199a6bb65df226f64d6139ae8aa98d83
Tonho <antonioreverso.com@gmail.com>
2025-05-06 16:51:54 -0300

    Feat: add boolean examples and condition checks to boolean.js

--- javascript/Javascript Masterclass/tipos_de_dados/boolean.js
+++ javascript/Javascript Masterclass/tipos_de_dados/boolean.js
@@ -1,2 +1,23 @@
 true;
 false;
+new Boolean(True);
+new Boolean(False);
+let condition = new Boolean(false);
+if (condition) {
+  console.log("The condition is true");
+} else {
+  console.log("The condition is false");
+}
+!!0;
+!!NaN;
+!!"";
+!!False;
+!!undefined;
+!!null;
+!!-10;
+!!"Javascript";
+!!{};
+!![];
+!!/javascript/;
+!!new Date();
+!!function () { };
