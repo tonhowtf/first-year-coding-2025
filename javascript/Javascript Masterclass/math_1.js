bad475751bef7c5034ffe38325a91cff54ad9ced
Tonho <antonioreverso.com@gmail.com>
2025-05-06 13:13:12 -0300

    Feat: add additional mathematical functions to math_1.js

--- javascript/Javascript Masterclass/math_1.js
+++ javascript/Javascript Masterclass/math_1.js
@@ -6,3 +6,23 @@ Math.LOG2E;
 Math.PI;
 Math.SQRT1_2;
 Math.SQRT2;
+Math.abs(10);
+Math.abs(-10);
+Math.ceil(1.1);
+Math.floor(9.9);
+Math.floor(-9.9);
+Math.round(4.5);
+Math.round(-4.5);
+Math.sign(5);
+Math.sign(-5);
+Math.trunc(2.3);
+Math.trunc(-2.3);
+Math.cbrt(8);
+Math.cos(Math.PI / 3);
+Math.exp(1);
+Math.hypot(3, 4);
+Math.log(1);
+Math.pow(2, 10)
+Math.sin(Math.PI / 2)
+Math.sqrt(4);
+Math.tan(Math.PI / 4)
