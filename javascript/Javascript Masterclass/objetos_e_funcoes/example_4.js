15e8fd0d7b90ea968b55b4ee02e0b7e7a55e5339
Tonho <antonioreverso.com@gmail.com>
2025-05-07 14:11:14 -0300

    Feat: add JavaScript object creation and assignment example in example_4.js

---
+++ javascript/Javascript Masterclass/objetos_e_funcoes/example_4.js
@@ -0,0 +1,10 @@
+const javascript = Object.create({});
+Object.assign(javascript, {
+  name: "Javascript",
+  year: 1995,
+  paradigm: "OO and Functional"
+}, {
+  author: "Brendan Eich",
+  influenceBy: "Java, Scheme and Self"
+});
+console.log(javascript);
