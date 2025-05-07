a265318b1f6a83a116a507685e48f6048ff1f7b4
Tonho <antonioreverso.com@gmail.com>
2025-05-07 10:15:47 -0300

    Feat: add book object examples and equality check in example_1.js and example_2.js

---
+++ javascript/Javascript Masterclass/objetos_e_funcoes/example_1.js
@@ -0,0 +1,11 @@
+const book = {
+  title: "Clean Code",
+  author: "robert C. Martin",
+  pages: 464,
+  language: "English",
+  available: true
+
+};
+
+book.available = undefined;
+console.log(book);
