b78ee202c2a35e7068ce623d04ac618c3267024e
Tonho <antonioreverso.com@gmail.com>
2025-05-11 16:06:10 -0300

    Refactor: comment out existing JavaScript object examples in example_4.js

--- javascript/Javascript Masterclass/objetos_e_funcoes/example_4.js
+++ javascript/Javascript Masterclass/objetos_e_funcoes/example_4.js
@@ -1,7 +1,30 @@
-const javascript = {
-  name: "Javascript",
-  year: 1995,
-  paradigm: "OO and Functional"
-};
-console.log(Object.keys(javascript));
-console.log(Object.values(javascript));
+// const javascript = Object.create({});
+// Object.assign(javascript, {
+//   name: "Javascript",
+//   year: 1995,
+//   paradigm: "OO and Functional"
+// }, {
+//   author: "Brendan Eich",
+//   influencedBy: "Java, Scheme"
+// });
+// console.log(Object.keys(javascript));
+// console.log(Object.values(javascript));
+//console.log(Object.is(javascript, javascript));
+
+// const javascript = {};
+// Object.defineProperty(javascript, "name", {
+//   value: "JavaScript"
+// });
+// console.log(javascript.name);
+
+// const javascript = {
+//   name: "JavaScript",
+//   year: 1995,
+//   paradigm: "OO and Functional"
+// };
+
+// Object.preventExtensions(javascript);
+// javascript.name = "ECMAScript";
+// javascript.author = "Brendan Eich";
+
+
