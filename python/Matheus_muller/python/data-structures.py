4430529a373cecb62b4dfa9fb27181dc19d0008a
Tonho <antonioreverso.com@gmail.com>
2025-05-22 00:18:22 -0300

    Refactor: comment out previous list operations and update name_list to a tuple

--- python/Matheus_muller/python/data-structures.py
+++ python/Matheus_muller/python/data-structures.py
@@ -1,6 +1,12 @@
-name_list = ["Tonho", "Bernadop", 12]
-name_list.append("Gabriel")
-name_list.remove("Bernadop")
+#name_list = ["Tonho", "Bernadop", 12]
+#name_list.append("Gabriel")
+#name_list.remove("Bernadop")
 
+#print(name_list)
+
+
+name_list = ("Tonhoca", "jococa", 55)
 print(name_list)
 
+
+name_list_set = set(name_list)
