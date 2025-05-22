7ce77d62e3f5360e1396c9fcc51705a6dec30b38
Tonho <antonioreverso.com@gmail.com>
2025-05-22 15:04:14 -0300

    Feat: add conditional statements to compare variables in conditions.py

--- python/Matheus_muller/python/conditions.py
+++ python/Matheus_muller/python/conditions.py
@@ -1,4 +1,9 @@
 a = 99
 b = 100
 
-if a >= b
+if a >= b:
+    print(f"{a} is greater than {b}")
+elif a == b:
+    print(f"{a} is equal to {b}")
+else:
+    print(f"{a} is less than {b}")
---
+++ python/Matheus_muller/python/loops.py
@@ -0,0 +1,17 @@
+nomes = ("Tonho", "Gamer", "Gotyberg")
+
+#print(nomes[0])
+
+"""for nome in nomes:
+    print(nome)"""
+
+"""for index in range(len(nomes)):
+    print(f"{index+1}: {nomes[index]}")"""
+
+
+"""count = 0
+
+while count < len(nomes):
+    print(nomes[count])
+    count+=1"""
+
