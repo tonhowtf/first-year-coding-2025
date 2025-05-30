af0fab73775c9e1363c466c999337f4b1de0f67e
Tonho <tonhowtf@gmail.com>
2025-05-29 22:13:55 -0300

    Feat: update Pessoas class to include age attribute and class methods for enhanced functionality

--- python/pythonandoo/POO/1_modelagem.py
+++ python/pythonandoo/POO/1_modelagem.py
@@ -1,10 +1,20 @@
 class Pessoas:
-    def __init__(self, nome):
+    possui_olho = True
+    possui_boca = True
+
+    def __init__(self, nome, idade):
+        self.idade = idade
         self.nome = nome
-        
+    
+    def retorna_nome(self):
+        return self.nome
+
     def logar_sistema(self):
         print(f'Estou logando no sistema')
+    @classmethod
+    def andar(cls):
+        print("Estou andando")
 
-p1 = Pessoas('tonho')
-
-print(p1.nome)
+#p1 = Pessoas('tonho', 25)
+#p2 = Pessoas('Tonhoca', 21)
+print(Pessoas.possui_boca)
