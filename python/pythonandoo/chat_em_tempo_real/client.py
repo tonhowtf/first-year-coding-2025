0e86b37be9f64622a2bf19e60cd60f7e02149b74
Tonho <tonhowtf@gmail.com>
2025-06-01 19:11:37 -0300

    Feat: implement basic chat client interface using Tkinter

---
+++ python/pythonandoo/chat_em_tempo_real/client.py
@@ -0,0 +1,32 @@
+import socket
+import threading
+from tkinter import *
+import tkinter
+from tkinter import simpledialog
+
+class Chat:
+    def __init__(self):
+        HOST = '127.0.0.1'
+        PORT = 55556
+        #self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #self.client.connect((HOST, PORT))
+        login = Tk()
+        login.withdraw()
+
+        self.janela_carregada = False
+        self.ativo = True
+
+        self.nome = simpledialog.askstring('Nome', 'Digite seu nome!', parent=login)
+        self.sala = simpledialog.askstring('Sala', 'Digite a sala que você deseja entrar!', parent=login)
+        self.janela()
+
+    def janela(self):
+        self.root = Tk()
+        self.root.geometry("800x800")
+        self.root.title('Chat')
+
+        self.caixa_texto = Text(self.root)
+        self.caixa_texto.place(relx=0.05, rely=0.01, width=700, height=600)
+        self.root.mainloop()
+
+chat = Chat()
