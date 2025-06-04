8693051117d65fd47d3ac2fdb24c290c02d05511
Tonho <tonhowtf@gmail.com>
2025-06-03 21:58:48 -0300

    Fix: update saudacao function to return String and modify main function to call it correctly

--- Kotlin/curso-kotlin/CursoDeKotlin/src/main/kotlin/Main.kt
+++ Kotlin/curso-kotlin/CursoDeKotlin/src/main/kotlin/Main.kt
@@ -2,18 +2,18 @@ import java.time.LocalDateTime
 
 fun main(){
 
-println(somar())
+println(saudacao(99))
 
 }
 
-fun saudacao(hora: Int): Int {
-    val horaDefault = LocalDateTime.now()
+fun saudacao(hora: Int): String =
+    when (hora) {
 
-    when(hora){
+
         in 6..11 -> "Bom dia"
         in 12..17 -> "Boa tarde"
         in 18..23 -> "Boa noite"
         in 0..5 -> "Boa madrugada"
-        else -> horaDefault
+        else -> "Horario Invalido"
     }
-}
+
