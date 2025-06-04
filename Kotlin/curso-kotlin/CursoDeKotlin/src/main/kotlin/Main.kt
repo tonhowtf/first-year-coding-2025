c17a75129fb8dd53ec07f3ed9cd68331f865b9ce
Tonho <tonhowtf@gmail.com>
2025-06-03 21:49:23 -0300

    Fix: correct return type of saudacao function and add missing return statement

--- Kotlin/curso-kotlin/CursoDeKotlin/src/main/kotlin/Main.kt
+++ Kotlin/curso-kotlin/CursoDeKotlin/src/main/kotlin/Main.kt
@@ -1,6 +1,19 @@
 import java.time.LocalDateTime
 
 fun main(){
-    var produto: String? = null
 
+println(somar())
+
+}
+
+fun saudacao(hora: Int): Int {
+    val horaDefault = LocalDateTime.now()
+
+    when(hora){
+        in 6..11 -> "Bom dia"
+        in 12..17 -> "Boa tarde"
+        in 18..23 -> "Boa noite"
+        in 0..5 -> "Boa madrugada"
+        else -> horaDefault
+    }
 }
