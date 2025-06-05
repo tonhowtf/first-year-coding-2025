eff2c026927298cc81dfca6bf9a922a0cd6b12d7
Tonho <tonhowtf@gmail.com>
2025-06-04 23:22:52 -0300

    Refactor: remove saudacao function and update main to create User instance

--- Kotlin/curso-kotlin/CursoDeKotlin/src/main/kotlin/Main.kt
+++ Kotlin/curso-kotlin/CursoDeKotlin/src/main/kotlin/Main.kt
@@ -1,19 +1,8 @@
 import java.time.LocalDateTime
 
-fun main(){
-
-println(saudacao(99))
-
+fun main() {
+    val user1 = User("Tonho", true)
 }
 
-fun saudacao(hora: Int): String =
-    when (hora) {
 
-
-        in 6..11 -> "Bom dia"
-        in 12..17 -> "Boa tarde"
-        in 18..23 -> "Boa noite"
-        in 0..5 -> "Boa madrugada"
-        else -> "Horario Invalido"
-    }
-
+class User(var name: String, var isAdmin: Boolean)
