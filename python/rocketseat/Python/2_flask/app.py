927df5d43ec29ef8172f6ed94a57057697433dc5
Tonho <tonhowtf@gmail.com>
2025-06-18 23:04:18 -0300

    Feat: implement task management API with create and retrieve functionality

---
+++ python/rocketseat/Python/2_flask/app.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, jsonify
+from models.task import Task
+
+
+# __name__ = "__main__"
+app = Flask(__name__)
+
+tasks = []
+task_id_control = 1
+
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    global task_id_control
+    data = request.get_json()
+    new_task = Task(id=task_id_control,title=data['title'], description=data.get("description", ""))
+    task_id_control += 1
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({"message": "Nova tarefa criada com sucesso"})
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+
+    task_list = [task.to_dict() for task in tasks]
+
+
+    output = {
+                "tasks": task_list,
+                "total_tasks": len(task_list)    
+    }
+
+    return jsonify(output)
+
+@app.route('/tasks/<int:id>', methods=['GET'])
+def get_task(id):
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.to_dict())
+    
+    return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
