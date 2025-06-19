ade2449af7fa54e8413ee93a9a4155add990a7cb
Tonho <tonhowtf@gmail.com>
2025-06-19 20:18:11 -0300

    Feat: enhance task creation response and add tests for task management

---
+++ python/rocketseat/Python/2_flask/tests.py
@@ -0,0 +1,46 @@
+import pytest
+import requests
+
+BASE_URL = "http://127.0.0.1:5000"
+
+tasks = []
+
+def test_create_task():
+    new_task_data = {
+        "title": "Nova tarefa",
+        "description": "Descrição da nova tarefa"
+    }
+    response = requests.post(f"{BASE_URL}/tasks", json=new_task_data)
+    assert response.status_code == 200
+    response_json = response.json()
+    assert "message" in response_json
+    assert "id" in response_json
+    tasks.append(response_json['id'])
+
+def test_get_tasks():
+    response = requests.get(f'{BASE_URL}/tasks')
+    assert response.status_code == 200
+    response_json = response.json()
+    assert "tasks" in response_json
+    assert "total_tasks" in response_json
+
+def test_get_task():
+    if tasks:
+        task_id = tasks[0]
+        response = requests.get(f'{BASE_URL}/tasks/{task_id}')
+        assert response.status_code == 200
+        response_json = response.json()
+        assert task_id == response_json['id']
+
+def test_update_task():
+    if tasks:
+        task_id = tasks[0]
+        payload = {
+            "completed": False,
+            "description": "Nova descrição",
+            "title": "Título atualizado"
+        }
+        response = requests.put(f"{BASE_URL}/tasks/{task_id}", json=payload)
+        response.status_code == 200
+        response_json = response.json()
+        assert "message" in response_json
